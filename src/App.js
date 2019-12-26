0ea997635ebf3dc2e66f129cd2fa6d4053234902
Lars Öltjenbruns <32353679+DatPunch@users.noreply.github.com>
2019-12-26 17:20:07 +0100

    Add user feedback animations & code cleanup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import css from "./App.module.css";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       generatedNote: null,
       hasWon: false,
       hasFailed: false
-    }
+    };
 
     this.notes = {
       treble: [
@@ -44,21 +44,38 @@ class App extends React.Component {
       ],
       bass: []
     };
+
+    this.displayText = React.createRef();
   }
 
-  componentDidMount = ()=>{
-    this.newNoteGuess()
-  }
+  componentDidMount = () => {
+    this.newNoteGuess();
+  };
 
-  getRandomArrayItem = (arr) =>{
+  getRandomArrayItem = arr => {
     return arr[Math.floor(Math.random() * arr.length)];
-  }
+  };
 
-  newNoteGuess= () => {
+  // From https://github.com/daneden/animate.css (modified)
+  animateCSS = (element, animationName, speed, callback) => {
+    const node = element;
+    node.classList.add("animated", animationName, speed);
+
+    function handleAnimationEnd() {
+      node.classList.remove("animated", animationName, speed);
+      node.removeEventListener("animationend", handleAnimationEnd);
+
+      if (typeof callback === "function") callback();
+    }
+
+    node.addEventListener("animationend", handleAnimationEnd);
+  };
+
+  newNoteGuess = () => {
     this.setState({
       hasWon: false,
       hasFailed: false
-    })
+    });
 
     const obj = this.getRandomArrayItem(this.notes.treble);
     const note = this.getRandomArrayItem(obj.representations);
@@ -68,52 +85,66 @@ class App extends React.Component {
         name: obj.name,
         value: note
       }
-    })
-  }
+    });
+  };
 
-  keyClickHandler= (e)=>{
+  keyClickHandler = e => {
+    if(this.state.hasWon) return
+
     const keyName = e.currentTarget.children[0].innerHTML;
     const correct = keyName === this.state.generatedNote.name ? true : false;
+    const displayTextEle = this.displayText.current;
 
-    if(correct){
-      this.setState({
-        hasWon: true
-      })
-
-      setTimeout(()=>{
-        this.newNoteGuess()
-      }, 2000)
-    } else {
+    if (!correct) {
       this.setState({
         hasFailed: true
-      })
+      });
+
+      this.animateCSS(displayTextEle, "tada", "fast")
+      return;
     }
-  }
+
+    this.setState({
+      hasWon: true
+    });
+
+    this.animateCSS(displayTextEle, "bounceIn", null)
+
+    setTimeout(() => {
+      this.newNoteGuess();
+    }, 2000);
+  };
 
   render() {
-    const keys = ["C","D","E","F","G","A","H"];
-    let text = this.state.hasWon? "Correct ✔" : "Which note is this?"
-    let textStyle= {
-      color: this.state.hasWon? "green" : "black"
-    }
+    const keys = ["C", "D", "E", "F", "G", "A", "H"];
+    let text = this.state.hasWon ? "Correct" : "Which note is this?";
+    let textStyle = {
+      color: this.state.hasWon ? "green" : "black"
+    };
 
-    if(this.state.hasFailed && !this.state.hasWon) text = "Try again"
+    if (this.state.hasFailed && !this.state.hasWon) text = "Try again";
 
     return (
       <div className={css.App}>
         <div className={css.displayWrapper}>
           <h1 className={css.display}>
-            {this.state.generatedNote? `'&====${this.state.generatedNote.value}======` : ""}
+            {this.state.generatedNote
+              ? `'&====${this.state.generatedNote.value}=====!`
+              : ""}
           </h1>
-        <h2 style={textStyle}>{text}</h2>
+          <h2 style={textStyle} ref={this.displayText}>
+            {text}
+          </h2>
         </div>
 
         <div className={css.keyboard}>
-          {
-            keys.map((val, i)=>{
-            return <div onClick={this.keyClickHandler} key={i}><p>{val}</p></div>
-            })
-          }
+          {keys.map((val, i) => {
+            return (
+              <div onClick={this.keyClickHandler} key={i}>
+                <p>{val}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
